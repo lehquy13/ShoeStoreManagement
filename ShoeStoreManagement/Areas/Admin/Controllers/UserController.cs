7c42f1e75117d019fa5647f3a95b83bacf3d1591
Kiệt Nguyễn Tuấn <motchut1106@gmail.com>
2022-11-03 23:10:29 +0700

    Add user load list.

--- ShoeStoreManagement/Areas/Admin/Controllers/UserController.cs
+++ ShoeStoreManagement/Areas/Admin/Controllers/UserController.cs
@@ -1,22 +1,40 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using ShoeStoreManagement.Areas.Identity.Data;
+using ShoeStoreManagement.Controllers;
+using ShoeStoreManagement.Core.Models;
+using ShoeStoreManagement.CRUD.Implementations;
+using ShoeStoreManagement.CRUD.Interfaces;
+using ShoeStoreManagement.Data;
 using System.Data;
+using System.Linq;
 
 namespace ShoeStoreManagement.Areas.Admin.Controllers
 {
-	public class UserController : Controller
-	{
-        [Area("Admin")]
-        [Authorize(Roles = "Admin")]
-        public IActionResult Index()
-		{
-            ViewBag.User = true;
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class UserController : Controller
+    {
+        private readonly ILogger<UserController> _logger;
+        private readonly IApplicationUserCRUD _applicationuserCRUD;
 
+        private List<ApplicationUser>? applicationUsers;
+
+        private void Init()
+        {
+            applicationUsers = _applicationuserCRUD.GetAllAsync().Result;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.ApplicationUser = true;
+            ViewData["applicationUser"] = applicationUsers;
             return View();
-		}
+        }
 
         public IActionResult Create()
         {
+            //ViewBag.Product = true;
             return View();
         }
     }
---
+++ ShoeStoreManagement/CRUD/Implementations/ApplicationUserCRUD.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShoeStoreManagement.Areas.Admin.Controllers;
+using ShoeStoreManagement.Areas.Identity.Data;
+using ShoeStoreManagement.Core.Models;
+using ShoeStoreManagement.CRUD.Interfaces;
+using ShoeStoreManagement.Data;
+
+namespace ShoeStoreManagement.CRUD.Implementations
+{
+    public class ApplicationUserCRUD : IApplicationUserCRUD
+  {
+        private readonly ApplicationDbContext _applicationDBContext;
+        public ApplicationUserCRUD(ApplicationDbContext applicationDBContext)
+        {
+            _applicationDBContext = applicationDBContext;
+        }
+
+        public async Task<List<ApplicationUser>> GetAllAsync()
+        {
+            return await _applicationDBContext.ApplicationUsers.ToListAsync();
+        }
+
+        public async Task<ApplicationUser?> GetByIdAsync(string id)
+        {
+            return await _applicationDBContext.ApplicationUsers.FindAsync(id);
+        }
+
+        public async Task CreateAsync(ApplicationUser applicationUser)
+        {
+            await _applicationDBContext.ApplicationUsers.AddAsync(applicationUser);
+            _applicationDBContext.SaveChanges();
+        }
+
+        public void Update(ApplicationUser updateApplicationUser)
+        {
+
+            if (updateApplicationUser != null)
+                _applicationDBContext.ApplicationUsers.Update(updateApplicationUser);
+            _applicationDBContext.SaveChanges();
+        }
+
+        public void Remove(ApplicationUser deleteApplicationUser)
+        {
+            if (deleteApplicationUser != null)
+                _applicationDBContext.ApplicationUsers.Remove(deleteApplicationUser);
+            _applicationDBContext.SaveChanges();
+        }
+  }
+}
